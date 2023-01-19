2f09aface07fed3fdbef364e8e7ed21c5efb66f3
anabatsh <nbatsheva@gmail.com>
2023-01-19 16:46:03 +0300

    UPD

--- new_method/lib.py
+++ new_method/lib.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.cm import get_cmap
 from gekko import GEKKO
 
 
@@ -14,8 +15,8 @@ def optimal_control_minimization(x_0, x_ref, time, f, F, integer=False):
         integer - integrality flag default False
     returns: 
         history: a dict with
-            x - predicted state trjectory of x(k)
-            i - best sequense of controls i(k)
+            x - predicted state trjectory of x(t)
+            i - best sequense of controls i(t)
             obj_val - corresponding objective value F(x, i)
         and some other items
     """
@@ -49,11 +50,11 @@ def optimal_control_substituting(x_0, x_ref, time, f, F, const_i):
         time - time horizon
         f - differential equation
         F - objective function
-        const_i - fixed sequence of i(k)
+        const_i - fixed sequence of i(t)
     returns:
         history: a dict with
-            x - predicted state trjectory of x(k)
-            i - given sequense of controls i(k)
+            x - predicted state trjectory of x(t)
+            i - given sequense of controls i(t)
             obj_val - corresponding objective value F(x, i)
         and some other items
     """
@@ -112,15 +113,32 @@ def show(history):
     
     print(f'Objective value = {obj_val:.6f}')
     
-def show_X(X, x_0, x_ref, time):
-    plt.figure(figsize=(7, 3))
-    plt.title('State x', fontsize=10)
-    plt.hlines(x_0, time[0], time[-1], colors='r', linestyles='dashed', label=r'$x_0$')
-    plt.hlines(x_ref, time[0], time[-1], colors='g', linestyles='dashed', label=r'$x_{ref}$')
-    for x in X:
-        plt.plot(time, x, '-o', c='r', markersize=4, linewidth=1)
-    plt.xticks(time, labels = ['' if i % 2 else round(t, 2) for i, t in enumerate(time)])
-    plt.xlabel('Time')
-    plt.grid(alpha=0.5)
-    plt.legend(loc=1)
+def show_X(X, x_0, x_ref, time, w, p):
+    """
+    """
+    cmap = get_cmap('plasma')
+    colors = cmap(w)
+    time_labels = ['' if i % 2 else round(t, 2) for i, t in enumerate(time)]
+    
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(7, 4), height_ratios=[1, 3])
+    plt.subplots_adjust(hspace=0.25)
+    
+    ax1.set_title('Probabilities p', fontsize=10)
+#     ax1.bar(time, p, width=0.03)
+    ax1.imshow([p], cmap='gray', vmin=0.0, vmax=1.0)
+    ax1.set_yticks([])
+    ax1.set_xticks(range(len(time)))
+    ax1.set_xticklabels(time_labels)
+    
+    ax2.set_title('State x', fontsize=10)
+    ax2.hlines(x_0, time[0], time[-1], colors='r', linestyles='dashed', label=r'$x_0$')
+    ax2.hlines(x_ref, time[0], time[-1], colors='g', linestyles='dashed', label=r'$x_{ref}$')
+    for x, c in zip(X, colors):
+        ax2.plot(time, x, '-o', c=c, markersize=4, linewidth=1)
+    ax2.set_xticks(time)
+    ax2.set_xticklabels(time_labels)
+    ax2.set_xlabel('Time')
+    ax2.grid(alpha=0.5)
+    ax2.legend(loc=1)
+    
     plt.show()
